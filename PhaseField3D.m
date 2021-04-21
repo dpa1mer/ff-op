7ea5efc71f02cf7384b5bac195bbd9c9c67304b4
David R. Palmer <drp@mit.edu>
2021-04-20 17:37:01 -0700

    Fix major bug in boundary conditions in 3D.

--- PhaseField3D.m
+++ PhaseField3D.m
@@ -1,5 +1,8 @@
-% Uses 0-Neumann Boundary Conditions
-function [Op, star0lump] = PhaseField3D(meshData, q, ellipticity)
+function [Op, star0lump] = PhaseField3D(meshData, q, ellipticity, neumann)
+
+if nargin < 4
+    neumann = true;
+end
 
 if nargin < 3
     ellipticity = 0.01;
@@ -26,8 +29,6 @@ nb = length(bdryIdx);
 %% Convert frames to tensors
 frameTensors = reshape(Monomial2Tensor(Sph024ToMonomial(Octa2Odeco(q))), 9, 9, nv);
 Tij = eye(9) - (1 - ellipticity) * (frameTensors ./ 24);
-
-% VisualizeFrameField(meshData, frames);
 
 %% Construct phase field operators
 volumes = TetVolumes(verts, tets);
@@ -62,25 +63,25 @@ V = spdiags(repelem(volumes, 3, 1), 0, 3 * nt, 3 * nt);
 star0lump = sparse(tets, tets, repmat(volumes ./ 4, 1, 4), nv, nv);
 M9 = kron(star0lump, speye(9));
 
-% weightedT = reshape(full(diag(star0lump)), 1, 1, nv) .* Tij;
+if neumann
+    %% Impose 0-Neumann Boundary Conditions (weakly)
+    bdryNormals = reshape(meshData.bdryNormals', 3, 1, nb);
+    nn = batchop('mult', bdryNormals, bdryNormals, 'N', 'T');
+    nnn = batchop('mult', bdryNormals, reshape(nn, 1, 9, nb));
+    multN = [multitransp(bdryNormals) zeros(1, 6, nb);
+             zeros(1, 3, nb) multitransp(bdryNormals) zeros(1, 3, nb);
+             zeros(1, 6, nb) multitransp(bdryNormals)];
+    multNt = multitransp(reshape(multN, 9, 3, nb));
+    Bij = [multN - nnn; multNt - nnn];
+    BT = batchop('mult', Bij, Tij(:,:,bdryIdx));
+    BTB = batchop('mult', BT, Bij, 'N', 'T');
+    BTBi = batchop('pinv', BTB, 4);
+    Tij(:,:,bdryIdx) = Tij(:,:,bdryIdx) - batchop('mult', batchop('mult', BT, BTBi, 'T', 'N'), BT, 'N', 'N');
 
-%% Impose 0-Neumann Boundary Conditions (weakly)
-bdryNormals = reshape(meshData.bdryNormals, 3, 1, nb);
-nn = batchop('mult', bdryNormals, bdryNormals, 'N', 'T');
-nnn = batchop('mult', bdryNormals, reshape(nn, 1, 9, nb));
-multN = [multitransp(bdryNormals) zeros(1, 6, nb);
-         zeros(1, 3, nb) multitransp(bdryNormals) zeros(1, 3, nb);
-         zeros(1, 6, nb) multitransp(bdryNormals)];
-multNt = multitransp(reshape(multN, 9, 3, nb));
-Bij = [multN - nnn; multNt - nnn];
-BT = batchop('mult', Bij, Tij(:,:,bdryIdx));
-BTB = batchop('mult', BT, Bij, 'N', 'T');
-BTBi = batchop('pinv', BTB, 5);
-Tij(:,:,bdryIdx) = Tij(:,:,bdryIdx) - batchop('mult', batchop('mult', BT, BTBi, 'T', 'N'), BT, 'N', 'N');
-
-% Bi = repmat(reshape(1:6*nb, 6, 1, nb), 1, 9, 1);
-% Bj = repmat(reshape((9 * bdryIdx + (-8:0)).', 1, 9, nb), 6, 1, 1);
-% B = sparse(Bi(:), Bj(:), Bij(:), 6 * nb, 9 * nv);
+    Bi = repmat(reshape(1:6*nb, 6, 1, nb), 1, 9, 1);
+    Bj = repmat(reshape((9 * bdryIdx + (-8:0)).', 1, 9, nb), 6, 1, 1);
+    B = sparse(Bi(:), Bj(:), Bij(:), 6 * nb, 9 * nv);
+end
 
 %% Form frame field principal symbol matrix
 vertBaseIdx = reshape(9 * (0:nv - 1), 1, 1, nv);
@@ -89,54 +90,15 @@ T_J = repmat(vertBaseIdx + (1:9), 9, 1, 1, 1);
 T = sparse(T_I(:), T_J(:), Tij(:), 9 * nv, 9 * nv);
 TM = T / M9;
 
-% % Only take interior nodes
-% intBlkIdx = (9 * intIdx + (-8:0)).';
-% D = D(:, intBlkIdx);
-% M9 = M9(intBlkIdx, intBlkIdx);
-% MT = MT(intBlkIdx, intBlkIdx);
+if ~neumann
+    %% Only take interior nodes
+    intBlkIdx = (9 * meshData.intIdx + (-8:0)).';
+    D = D(:, intBlkIdx);
+    TM = TM(intBlkIdx, intBlkIdx);
+end
 
-% %% Impose 0-Neumann boundary conditions
-% 
-% 
-% faceEdgeLengths = reshape(faceEdgeLengths.', 3, 1, nf);
-% [bdryFaceIdx, ~] = find(B.');
-% bdryFaces = faces(bdryFaceIdx, :);
-% [bdryFaceEdgeIdx, ~] = find((bdryEdgeIdx == face2edge(bdryFaceIdx, :)).');
-% bdryFaceEdgeIdx = reshape(bdryFaceEdgeIdx, [], 1);
-% bdryFaceShift = mod(bdryFaceEdgeIdx + (-2:0), 3) + 1;
-% bdryFaceShift = sub2ind(size(bdryFaces), repmat((1:nb).', 1, 3), bdryFaceShift);
-% bdryFaces = reshape(bdryFaces(bdryFaceShift), nb, 3);
-% bdryFaceVertAngles = vertAngles(bdryFaceIdx, :);
-% bdryFaceVertAngles = reshape(bdryFaceVertAngles(bdryFaceShift), nb, 3);
-% bdryFaceLengths = squeeze(faceEdgeLengths(:, :, bdryFaceIdx)).';
-% bdryFaceLengths = reshape(bdryFaceLengths(bdryFaceShift), nb, 3);
-% 
-% % vk = bdryFaces(:, 1);
-% bdryBasis = sparse(bdryFaces(:, [1 1]), bdryFaces(:, [3 2]), cos(bdryFaceVertAngles(:, [2 3])) .* bdryFaceLengths(:, [1 3]) ./ bdryFaceLengths(:, 2), nv, nv);
-% % allButVk = setdiff((1:nv).', vk);
-% % bdryBasis = bdryBasis(:, allButVk);
-% bdryNeighborCount = sum(bdryBasis ~= 0, 2);
-% constrainedNodes = bdryNeighborCount == 2;
-% bdryBasis(~constrainedNodes, :) = 0;
-% otherIdent = speye(nv);
-% otherIdent(constrainedNodes, :) = 0;
-% bdryBasis = bdryBasis + otherIdent;
-% bdryBasis = bdryBasis(:, ~constrainedNodes);
-
-%% Compute eigenfunctions
+%% Build final operator
 DVG = D' * V * G;
 Op = DVG' * TM * DVG;
-% O = bdryBasis.' * O * bdryBasis;
-% M = bdryBasis.' * star0 * bdryBasis;
-% [V, lambda] = eigs(Op + star0lump, star0lump, 200, 'smallestabs');%, 'IsSymmetricDefinite', true);
-% % V = bdryBasis * V;
-% % [W, ~] = eigs(L, star0, 100, 'smallestabs');
-% figure;
-% % k = 200;
-% % [cmin, cmax] = bounds(V(:, 1:64), 'all');
-% for k = 1:64
-%     subplot(8, 8, k);
-%     trisurf(meshData.bdry, V(:, k)); view(3); axis image vis3d off; shading interp; colormap viridis;
-% end
 
 end
