69d99124cc220d5d693ae3de1e30b7b805b3aa3d
David R. Palmer <drp@mit.edu>
2021-04-24 10:32:44 -0700

    MultiscaleExperiment3D with teddy.

--- MultiscaleExperiment3D.m
+++ MultiscaleExperiment3D.m
@@ -1,16 +1,23 @@
-function [meanEdgeLengths, evs] = MultiscaleExperiment3D()
+function [meanEdgeLengths, evs] = MultiscaleExperiment3D(colormap)
 
-meshfiles = ["meshes/spheres/sphere_r0.15.mesh";
-             "meshes/spheres/sphere_r0.14.mesh";
-             "meshes/spheres/sphere_r0.13.mesh";
-             "meshes/spheres/sphere_r0.12.mesh";
-             "meshes/spheres/sphere_r0.11.mesh";
-             "meshes/spheres/sphere_r0.10.mesh";
-             "meshes/spheres/sphere_r0.09.mesh";
-             "meshes/spheres/sphere_r0.08.mesh";
-             "meshes/spheres/sphere_r0.07.mesh";
-             "meshes/spheres/sphere_r0.06.mesh";
-             "meshes/spheres/sphere_r0.05.mesh"];
+% meshfiles = ["meshes/spheres/sphere_r0.15.mesh";
+%              "meshes/spheres/sphere_r0.14.mesh";
+%              "meshes/spheres/sphere_r0.13.mesh";
+%              "meshes/spheres/sphere_r0.12.mesh";
+%              "meshes/spheres/sphere_r0.11.mesh";
+%              "meshes/spheres/sphere_r0.10.mesh";
+%              "meshes/spheres/sphere_r0.09.mesh";
+%              "meshes/spheres/sphere_r0.08.mesh";
+%              "meshes/spheres/sphere_r0.07.mesh";
+%              "meshes/spheres/sphere_r0.06.mesh";
+%              "meshes/spheres/sphere_r0.05.mesh";
+%              "meshes/spheres/sphere_r0.04.mesh"];
+% %              "meshes/spheres/sphere_r0.03.mesh";
+% %              "meshes/spheres/sphere_r0.02.mesh"];
+
+meshfiles = ["../meshes/dbommes-meshes/teddy_80k.mesh";
+             "../meshes/dbommes-meshes/teddy_200k.mesh";
+             "../meshes/dbommes-meshes/teddy_370k.mesh"];
 
 levels = length(meshfiles);
 
@@ -20,7 +27,7 @@ for level = 1:levels
 end
 
 %% Compute frame field at finest level and average up
-q{levels} = MBO(meshData{levels}, RayMBO);
+q{levels} = MBO(meshData{levels}, RayMBO, [], 50, 3);
 octa = OctaMBO;
 q{levels} = octa.proj(q{levels});
 qT = q{levels}';
@@ -31,7 +38,7 @@ for level = (levels-1):-1:1
     q{level} = zeros(9, meshData{level}.nv);
     q{level}(:, insideIx) = squeeze(sum(bary(insideIx, :) .* reshape(qT(meshData{levels}.tets(tetIx(insideIx), :), :), [], 4, 9), 2))';
     q{level}(:, insideIx) = octa.proj(q{level}(:, insideIx));
-    q{level} = MBO(meshData{level}, RayMBO, q{level});
+    q{level} = MBO(meshData{level}, RayMBO, q{level}, 1, 3);
     q{level} = octa.proj(q{level});
 end
 
@@ -45,13 +52,17 @@ end
 %% Solve eigenvalue problem
 for level = 1:levels
     [V, D] = eigs(Op{level} + 1e-6 * M{level}, M{level}, 64, 'smallestabs');
-    figure('Name', sprintf('Level %d : mean edge length %d', level, meanEdgeLengths(level)), 'NumberTitle', 'off');
-    plot_3df(meshData{level}.verts, meshData{level}.tets, V(:, 64), 'PlotType', 'CleanSlice');
+    [~, maxXIdx] = max(meshData{level}.verts(:, 1)); % Consistent sign: make EFs positive here
+    fig = figure('Name', sprintf('Level %d : mean edge length %d', level, meanEdgeLengths(level)), 'NumberTitle', 'off');
+    FancyScatter(meshData{level}.verts, V(:, 32) * sign(V(maxXIdx, 32)), colormap, 0, 2); view(30, 70); axis image vis3d off;
+    fig.WindowState = 'maximized';
     evs(:, level) = diag(D);
 end
 
+VisualizeFrameField(meshData{levels}, q{levels});
+
 figure('Name', 'Eigenvalue Convergence');
-bar(evs(:, 1:levels-1) - evs(:, levels));
+plot(evs);
 legend('Location', 'northwest');
 
 end
